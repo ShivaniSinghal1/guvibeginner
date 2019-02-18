8cb61b6c9d1d44296633eb31ac517c29e4db4683
ShivaniSinghal1 <43058544+ShivaniSinghal1@users.noreply.github.com>
2019-02-18 03:35:41 -0800

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+a=input("enter the number")
+b=(a=='A' or a=='E' or a=='I' or a=='O' or a=='U')
+c=(a=='a' or a=='e' or a=='i' or a=='o' or a=='u')
+if(b or c):
+  print("character is vowel")
+else:
+   print("character is consonent")
