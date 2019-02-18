f80cec7c5c017419bc422cbd379c4b4af5994aba
ShivaniSinghal1 <43058544+ShivaniSinghal1@users.noreply.github.com>
2019-02-18 03:56:39 -0800

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,10 @@
+num= int(input("enter the value of n:")
+hold=num
+sum=0
+if num>0:
+  while num>0:
+    sum=sum+num
+    num=num+1;
+else:
+  print("enter the whole positive number")
+print("sum of first",hold,"natural number is:",sum) 
